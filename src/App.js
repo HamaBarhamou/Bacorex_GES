61bf86421f768544c87adb35b8f3300d21639d60
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-27 15:29:01 +0100

    add header 42

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   App.js                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Barhamou <hamabarhamou@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 14:15:49 by Barhamou          #+#    #+#             */
+/*   Updated: 2022/09/27 15:27:19 by Barhamou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Connexion from "./pages/Connexion";
-import Work_page from "./pages/Work_page";
+
 
 function App() {
   return (
