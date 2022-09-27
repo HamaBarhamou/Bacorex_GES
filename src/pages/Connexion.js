577bf8a8e79d893e3e5d65bae853324c885acb0e
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-27 15:57:45 +0100

    retoutn

--- src/pages/Connexion.js
+++ src/pages/Connexion.js
@@ -6,19 +6,19 @@
 /*   By: Barhamou <hamabarhamou@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 15:27:02 by Barhamou          #+#    #+#             */
-/*   Updated: 2022/09/27 15:27:04 by Barhamou         ###   ########.fr       */
+/*   Updated: 2022/09/27 15:56:59 by Barhamou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import React from 'react';
 
-
-const Connexion = () => {
-    return (
-        <div>
-            <h2>Ma page de connexion</h2>
-        </div>
-    );
+const Connexion = () => {   
+  return (
+    <div className="app">
+     <h2>my connexion page</h2>
+    </div>
+  );
 };
+
 
 export default Connexion;
