711752e11fd7eb2ee53a23d5cfb25ac0a91ac6b8
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-12 16:45:09 +0100

    implementation menue

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -10,16 +10,30 @@ const style ={
 const Menus = () => {
     return (
         <div style={style}>
-           <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
-            <MenuItem>New File</MenuItem>
-            <MenuItem>Save</MenuItem>
-            <SubMenu label="Edit">
-                <MenuItem>Cut</MenuItem>
-                <MenuItem>Copy</MenuItem>
-                <MenuItem>Paste</MenuItem>
-            </SubMenu>
-            <MenuItem>Print...</MenuItem>
-        </Menu>
+            <Menu menuButton={<MenuButton> Menu</MenuButton>}>
+                <SubMenu label="Generale">
+                    <MenuItem>Affaire</MenuItem>
+                    <MenuItem>Position</MenuItem>
+                    <MenuItem>Intervenants</MenuItem>
+                    <MenuItem>Planification</MenuItem>
+                    <MenuItem>Travaux a faire</MenuItem>
+                    <MenuItem>Caracteristique Technique</MenuItem>
+                </SubMenu>
+                <SubMenu label="Suivie">
+                    <MenuItem>Retroplannig</MenuItem>
+                    <MenuItem>Liste des modifications</MenuItem>
+                    <MenuItem>Revue portefeuille</MenuItem>
+                    <MenuItem>Alerte</MenuItem>
+                    <MenuItem>Achat</MenuItem>
+                    <MenuItem>Courier</MenuItem>
+                    <MenuItem>Clients</MenuItem>
+                    <MenuItem>Pieces Joint</MenuItem>
+                    <MenuItem>Ajour agent</MenuItem>
+                    <MenuItem>nouveau courier</MenuItem>
+                    <MenuItem>Rechercher</MenuItem>
+                    <MenuItem>CRET</MenuItem>
+                </SubMenu>
+            </Menu>
                     
         </div>
     );
